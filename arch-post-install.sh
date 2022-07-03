9fc0cb3e8cb6d6038d452b9eedc6d3b4fcb8f947
Paulo Lucas <76008216+PauLuke@users.noreply.github.com>
2022-07-03 11:25:29 -0300

    Add files via upload

---
+++ arch-post-install.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+#
+# Update and install the tools needed
+#
+sudo pacman -Syu base-devel git
+
+#
+# Create a directory for git
+#
+mkdir -p ~/.git
+
+#
+# Enter the created directory
+#
+cd ~/.git
+
+#
+# Install paru from Github
+#
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+
+#
+# Install my essential packages
+#
+paru -Syu firefox ttf-font-awesome kitty ruby-fusuma ttf-font-awesome brightnessctl pamixer wtype acpi grimshot tlp vlc wofi-wifi-menu-git fish
+
+#
+# Bring my config files
+#
+cd ~/
+git clone https://github.com/PauLuke/dotfiles
+cp -a ~/dotfiles/. ~/.config
+
+#
+# Rebooting the system
+#
+reboot
