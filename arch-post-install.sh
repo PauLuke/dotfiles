95c15b6f831a81478fa190354c99159cbe952588
Paulo Lucas <76008216+PauLuke@users.noreply.github.com>
2022-07-19 09:59:09 -0300

    Update arch-post-install.sh

--- arch-post-install.sh
+++ arch-post-install.sh
@@ -25,7 +25,7 @@ makepkg -si
 #
 # Install my essential packages
 #
-paru -Syu firefox ttf-font-awesome kitty ruby-fusuma ttf-font-awesome brightnessctl pamixer wtype acpi grimshot tlp vlc wofi-wifi-menu-git fish
+paru -Syu firefox ttf-font-awesome kitty ruby-fusuma ttf-font-awesome brightnessctl pamixer wtype acpi grimshot tlp vlc fish
 
 #
 # Bring my config files
